a25afc4568392bf596df5ac12f02ab6819909220
Leo Harada <leotharada@gmail.com>
2023-10-27 12:04:46 -0700

    updating email services eith EmailJS

--- public/main.js
+++ public/main.js
@@ -1,17 +1,17 @@
-const contactForm = document.querySelector('#contact-form');
-const submitButton = document.querySelector('.submit-button');
-const subjectInput = document.querySelector('#subject')
-const nameInput = document.querySelector('#user_name');
-const emailInput = document.querySelector('#user_email');
-const messageInput = document.querySelector('#message');
+const contactForm = document.querySelector("#contact-form");
+const submitButton = document.querySelector(".submit-button");
+const subjectInput = document.querySelector("#subject");
+const nameInput = document.querySelector("#user_name");
+const emailInput = document.querySelector("#user_email");
+const messageInput = document.querySelector("#message");
 
-const publicKey = 'MiFGG61-WxKnTxZ7f';
-const serviceId = 'service_e6gftku';
-const templateId = 'template_1aas5jr';
+const publicKey = "28P63q7oWBplk5FI-";
+const serviceId = "service_e6gftku";
+const templateId = "template_1aas5jr";
 
 emailjs.init(publicKey);
 
-contactForm.addEventListener('submit', e => {
+contactForm.addEventListener("submit", (e) => {
     e.preventDefault();
     submitButton.innerText = "Sending...";
 
@@ -19,19 +19,21 @@ contactForm.addEventListener('submit', e => {
         subject: subjectInput.value,
         name: nameInput.value,
         email: emailInput.value,
-        message: messageInput.value
-    }
+        message: messageInput.value,
+    };
 
-    emailjs.send(serviceId, templateId, inputFields)
-    .then(() => {
-        submitButton.innerText = "Sent!";
+    emailjs.send(serviceId, templateId, inputFields).then(
+        () => {
+            submitButton.innerText = "Sent!";
 
-        subjectInput.value = "";
-        nameInput.value = "";
-        emailInput.value = "";
-        messageInput.value = "";
-    }, (error) => {
-        console.log(error);
-        submitButton.innerText = "Something went wrong";
-    });
+            subjectInput.value = "";
+            nameInput.value = "";
+            emailInput.value = "";
+            messageInput.value = "";
+        },
+        (error) => {
+            console.log(error);
+            submitButton.innerText = "Something went wrong";
+        }
+    );
 });
